b82c4c4c2f94fb316a3560c46a311432540f371a
emilyhmoore <emily.moore@wustl.edu>
2014-08-06 15:08:16 -0500

    Lab 3 Stuff

--- day3/lab3.py
+++ day3/lab3.py
@@ -1,48 +1,30 @@
 def shout(txt):
   new_txt = txt.upper()
-  new_txt = new_txt.replace(". ", "!")
-  if new_txt[len(new_txt) - 1] != ".":
-    new_txt = new_txt + "!"
-  new_txt = new_txt.replace("?", "!")
+  new_txt = new_txt.replace(".", "")
+  new_txt = new_txt.replace("?", "")
+  new_txt = new_txt.replace("!", "")
+  new_txt = new_txt + "!"
   return new_txt
   
 def reverse(txt):
   if isinstance(txt, str) == False:
     return ""
       
-  return txt[1::-1]
+  return txt[::-1]
   
 def reversewords(txt):
   if isinstance(txt, str) == False:
     return ""
   
-  new_text = ""
-  reversed_sentences = []
-    
-  tmp = txt.replace("?", ".")
-  tmp = tmp.replace("!", ".")
-  sentences = tmp.split(". ")
-  sentences = [s.strip() for s in sentences if len(s.strip()) > 0]
-  
-  last_sentence = sentences[len(sentences) - 1]
-  if last_sentence[len(last_sentence) - 1] == ".":
-    sentences[len(sentences) - 1] = last_sentence[0:len(last_sentence)-1]
-  
-  for sentence in sentences:
-    words = sentence.split()
-    words.reverse()
-    reversed_sentence = ""
-    for word in words:
-      reversed_sentence += word
-      reversed_sentence += " "
-    reversed_sentences.append(reversed_sentence[0:(len(reversed_sentence)-1)])
-  
-  for sentence in reversed_sentences:
-    if len(sentence) > 0:
-      new_text += sentence
-      new_text += ". "
-    
-  return new_text
+  txt=txt.replace(".", " .")
+  txt = txt.replace("?", " ?")
+  txt = txt.replace("!", " !")
+  txt = txt.replace(",", " ,")
+  new_text = str.split(txt, " ")
+  word_list=new_text[::-1]
+  reversed_sentences = ' '.join(word_list)
+
+  return reversed_sentences
   
 def reversewordletters(txt):
   if isinstance(txt, str) == False:
--- day3/lab3_test.py
+++ day3/lab3_test.py
@@ -8,6 +8,22 @@ class Lab3Test(unittest.TestCase):
 
 	def test_shout_abc(self):
 		self.assertEqual("ABC!", lab3.shout("abc")) 
-
+	
+	def test_shout_punctuation(self):
+		self.assertEqual("CAT!", lab3.shout("cat.")) 
+	
+	def test_reverse_abc(self):
+		self.assertEqual("cba", lab3.reverse("abc"))
+		self.assertEqual(".cba", lab3.reverse("abc."))
+	
+	def test_reversewords(self):
+		self.assertEqual("? you hate I", lab3.reversewords("I hate you?"))
+		self.assertEqual("! Run", lab3.reversewords("Run!"))
+		self.assertEqual(". World Hello", lab3.reversewords("Hello World."))
+		self.assertEqual("? you are How . World Hello", lab3.reversewords("Hello World. How are you?"))
+	
+	def test_reverse_sentence_words(self):
+		self.assertEqual("nuR!", lab3.reversewordletters("Run!"))
+	
 if __name__=='__main__':
-	unittest.main
+	unittest.main()
