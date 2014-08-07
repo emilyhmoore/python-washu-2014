4925ff88eabbba854893678c173e37b49fe95ab2
emilyhmoore <emily.moore@wustl.edu>
2014-08-07 13:32:53 -0500

    School Homework
    Make School Work

--- day2/school.py
+++ day2/school.py
@@ -2,8 +2,25 @@ class School():
 	def __init__(self,name):
 		self.name=name
 		self.db={}
+	
 	def add(self, student, grade):
 		student_set=set()
+		if self.db.has_key(grade):
+			student_set=self.db[grade]
 		student_set.add(student)
-		self.db[grade]=student_set #creats a set
-		
+		self.db[grade]=student_set
+		return self.db
+	
+	def grade(self, grade):
+		if self.db.has_key(grade):
+			return self.db[grade]	
+		else:
+			return None
+	
+	def sort(self):
+		sorted_students=[]
+		for i in range(len(sorted(self.db))):
+			sorted_students.append(tuple(self.db[sorted(self.db)[i]])) ##make a list of the students in order
+		sorted_db=dict(zip(sorted(self.db),sorted_students)) ##zip them into a dictionary
+		return sorted_db
+			
--- day3/teen.py
+++ day3/teen.py
@@ -2,13 +2,14 @@ class Bob():
 	def __init__(self):
 		pass
 	
+	@classmethod
 	def askbob(self, txt):
 		is_question = "?" in txt
 		answer="Whatever"
 		if is_question:
 			answer="Sure."
-		if txt==txt.upper(txt):
+		if txt==txt.upper():
 			answer="Whoah, chill out!"
+		#if txt == ("")
+		#	answer="Fine. Be that way!"
 		return answer
-	
-		
