594b4b506003e6a47d707ab7f8a68d41e94b48bd
emilyhmoore <emily.moore@wustl.edu>
2014-08-16 09:15:37 -0500

    update hw4
    fixed some problems with the functions. Still not complete.

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -12,19 +12,29 @@ class LinkedList:
 		self.head=Node(value)
 		self.tail=self.head
 		self.size=1
+	
 	def length(self): #Returns the length of the list
 		return self.size
 	
-	def addNode(self, new_value): #Takes a number and adds it to the end of the list
+	def addNode(self, new_value): 
+	#Takes a number and adds it to the end of the list
 		node=Node(new_value, None)
-        self.tail.next = node
-        self.tail = node
+		self.tail.next = node
+		self.tail = node
 		self.size+=1
 		
 	def addNodeAfter(self, new_value, after_node): 
 	#Takes a number and adds it after the after_node
-		new_value.next = after_node.next
-		after_node.next = new_value
+		new_node=Node(new_value)
+		node=self.head
+		location=after_node-1
+		count = 0
+		while count != location:
+			node = node.next
+			count+=1
+		new_node.next=node.next
+		node.next = new_node
+		self.size+=1
 	
 	def addNodeBefore(self, new_value, before_node): 
 	#Takes a value and adds before the before_node
@@ -33,13 +43,13 @@ class LinkedList:
 			loc = loc.next
 		addNodeAfter(new_value, loc)
 	
-	def removeNode(self, node_to_remove): #Removes a node from the list
+	#def removeNode(self, node_to_remove): #Removes a node from the list
 	
-	def removeNodesByValue(self, value): #Takes a value, removes all nodes with that value
+	#def removeNodesByValue(self, value): #Takes a value, removes all nodes with that value
 	
-	def reverse(self): #Reverses the order of the linked list
+	#def reverse(self): #Reverses the order of the linked list
 	
-	def __str__(self): #Displays the list in some reasonable way
+	#def __str__(self): #Displays the list in some reasonable way
 	
 	
 	
