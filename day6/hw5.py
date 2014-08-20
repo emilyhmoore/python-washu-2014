254d3a00df71aead7fdae52941383fab26408535
emilyhmoore <emily.moore@wustl.edu>
2014-08-20 09:30:33 -0500

    update hw5
    still need to do is-post and comments

--- day6/hw5.py
+++ day6/hw5.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import csv
 from nltk.util import clean_html
 import urllib2
+import re
 
 # What page?
 page_to_scrape = 'http://www.washingtonpost.com/blog/monkey-cage/Archive/201408'
 
 # What info do we want?
-headers = ["Title", "Author"]
+headers = ["Title", "Author", "Publish Date", "URL","Is_Post","Comments"]
 
 # Where do we save info?
 filename = "monkey-cage-blogs.csv"
@@ -25,28 +26,41 @@ soup.prettify()
 
 # Extract titles on page
 titles = soup.findAll("h2", attrs={'class':'entry-title'})
-print len(titles)
+#print len(titles)
 for title in titles:
 	p = clean_html(str(title.find("a")))
-	print p
+#	print p
+
 authors = soup.findAll("div", attrs={'class':'blog-byline'})
-print len(authors)
+#print len(authors)
 for author in authors:
 	s = clean_html(str(author))
 	s = "".join(s.split("By ")[1::])
-	print s
+#	print s
+
 dates = soup.findAll("span", attrs={'class':'timestamp'})
-print len(dates)
+#print len(dates)
 for date in dates:
 	d = clean_html(str(date))
 	d = "".join(d.split("Posted at ")[1::])
-	print d
+#	print d
 	
-for i in range(47):
+theurls = soup.findAll("a", attrs={'href':re.compile("^http://www.washingtonpost.com/blogs/monkey-cage/wp/2014/08")})
+theurls=theurls[::3]
+for i in range(len(theurls)):
+	u=theurls[i]
+	u=u["href"]
+#	print u
+	
+for i in range(51):
 	title = titles[i]
 	p = clean_html(str(title.find("a")))
 	author = authors[i]
 	s ="".join(clean_html(str(author)).split("By ")[1::])
-	csvwriter.writerow([p, s])
+	date=dates[i]
+	d="".join(clean_html(str(date)).split("Posted at ")[1::])
+	url=theurls[i]["href"]
+	u=theurls[i]["href"]
+	csvwriter.writerow([p, s, d, u])
 	
 readFile.close()
